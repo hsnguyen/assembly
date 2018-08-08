ac659cc8a08a0b80f386bc1847b52474f174875f
Son Nguyen <s.hoangnguyen@imb.uq.edu.au>
2018-08-08 18:17:27 +1000

    todo: improve cross-correlation of concatemer

--- src/main/java/org/rtassembly/experiment/RawConcatemer.java
+++ src/main/java/org/rtassembly/experiment/RawConcatemer.java
@@ -64,17 +64,58 @@ public class RawConcatemer {
 		
 		return retval;
 	}
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		RawConcatemer concat=new RawConcatemer("/home/sonhoanghguyen/Projects/concatemers/data/raw/imb17_013486_20171130__MN17279_sequencing_run_20171130_Ha_BSV_CaMV1_RBarcode_35740_read_17553_ch_455_strand.fast5");
-		ArrayList<Double> retval = concat.scan();
-		PrintWriter writer = new PrintWriter(new FileWriter("/home/sonhoanghguyen/Projects/concatemers/data/raw/concat7.signal")); 
-		writer.print(concat.name+" ");
-		for(double sig:retval)				
-			writer.printf("%.5f ",sig);
-		writer.println();
+	
+	public void printCrossCorrelation(String outFile) throws IOException {
+		PrintWriter writer = new PrintWriter(new FileWriter(outFile)); 
+		writer.print(name+" ");
+
+		int 	l=signal.length;
+//		System.out.println("length="+l);
+		/*
+		 * 				0			l1-1
+		 * 				|			|
+		 * template	: 	-------------
+		 * 							|
+		 * 							i
+		 * 							|
+		 * query	:			--------------------------------|-------------|	
+		 * 						|								|	
+		 *						0								l2-1		l1+l2-2
+		 *
+		 *	Overlap: template (l1-i,l1) <=> query (q_start,i)
+		 */
 		
+		for(int i=0;i<l-1;i++) {
+			System.out.print("n="+i+"/"+(2*l)+"...");
+			int 	q_start=(i>=l?i-l+1:0), //coordinate on query of the overlap starting point
+					q_end=(i<=l-1?i:l-1),	 //coordinate on query of the overlap ending point	
+					t_start=(i>=l?0:l-i-1), //coordinate on template of the overlap starting point
+					t_end=(i<=l-1?l-1:2*l-i-2); //coordinate on template of the overlap ending point
+			double score=0.0, value=0.0;
+			int overlap=q_end-q_start;
+			
+			if(overlap>0){
+				while(q_start<=q_end && t_start<=t_end)
+					score+=1.0/(1.0+Math.abs(signal[q_start++]-signal[t_start++]));
+				
+				value=score/overlap;
+			}
+			writer.printf("%.5f ",value); //normalized it
+
+//			System.out.println("f(n)="+(score/overlap));
+		}
+		writer.println();
 		writer.close();
+	}
+	
+	public static void main(String[] args){
+		RawConcatemer concat=new RawConcatemer("/home/sonhoanghguyen/Projects/concatemers/data/raw/imb17_013486_20171130__MN17279_sequencing_run_20171130_Ha_BSV_CaMV1_RBarcode_35740_read_17553_ch_455_strand.fast5");
+		try {
+			concat.printCrossCorrelation("/home/sonhoanghguyen/Projects/concatemers/data/raw/concat7.signal");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+
 	}
 
 }
