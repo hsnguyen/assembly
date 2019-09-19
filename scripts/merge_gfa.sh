f5400da4c577653b3e1b7d1a1415f99b50307a50
Son Nguyen <s.hoangnguyen@imb.uq.edu.au>
2019-09-19 13:15:28 +1000

    change scripts to reindex gfa

--- scripts/merge_gfa.sh
+++ scripts/merge_gfa.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <1.gfa> <2.gfa>"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <1.gfa> <2.gfa> ..."
 else
-	awk -f gfa_merge.awk $1 $2
+	DIR=`dirname $0`
+	ncount=0
+	fcount=0
+	for gfa in "$@"
+	do
+		fcount=$((fcount + 1))
+		awk -v id="$ncount" -f ${DIR}/gfa_reindex.awk ${gfa} > reindexed_${fcount}.gfa
+		sline=`cat reindexed_${gfa}|awk '/^S/{count++}END{print count}'`
+		ncount=$((ncount + sline))
+	done	
+	#concatenate all reindexed gfa files together
+	awk '{if(data[$1]=="") data[$1]=$$0; else data[$1]=data[$1]"\n"$0;}END{if(data["S"]!="") print data["S"]; if(data["L"]!="") print data["L"]; if(data["C"]!="") print data["C"]; if(data["P"]!="") print data["P"];}' reindexed_[1-${fcount}].gfa
 fi
