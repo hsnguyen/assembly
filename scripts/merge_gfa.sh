1b085ff3d0d0d5f355c000d6803885cb996f8d50
Son Nguyen <s.hoangnguyen@imb.uq.edu.au>
2019-09-19 16:37:10 +1000

    edit merge_gfa

--- scripts/merge_gfa.sh
+++ scripts/merge_gfa.sh
@@ -2,16 +2,17 @@
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <1.gfa> <2.gfa> ..."
 else
-	DIR=`dirname $0`
+	SDIR=`dirname $0`
+	FDIR=`dirname $1`
 	ncount=0
 	fcount=0
 	for gfa in "$@"
 	do
 		fcount=$((fcount + 1))
-		awk -v id="$ncount" -f ${DIR}/gfa_reindex.awk ${gfa} > reindexed_${fcount}.gfa
+		awk -v id="$ncount" -f ${SDIR}/gfa_reindex.awk ${gfa} > ${FDIR}/reindexed_${fcount}.gfa
 		sline=`cat reindexed_${gfa}|awk '/^S/{count++}END{print count}'`
 		ncount=$((ncount + sline))
 	done	
 	#concatenate all reindexed gfa files together
-	awk '{if(data[$1]=="") data[$1]=$$0; else data[$1]=data[$1]"\n"$0;}END{if(data["S"]!="") print data["S"]; if(data["L"]!="") print data["L"]; if(data["C"]!="") print data["C"]; if(data["P"]!="") print data["P"];}' reindexed_[1-${fcount}].gfa
+	awk '{if(data[$1]=="") data[$1]=$$0; else data[$1]=data[$1]"\n"$0;}END{if(data["S"]!="") print data["S"]; if(data["L"]!="") print data["L"]; if(data["C"]!="") print data["C"]; if(data["P"]!="") print data["P"];}' ${FDIR}/reindexed_[1-${fcount}].gfa
 fi
